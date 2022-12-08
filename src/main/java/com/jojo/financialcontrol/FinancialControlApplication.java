ff206dafba60197381a74e234c72ca9ba3114cc5
joao <joaovlopes000@gmail.com>
2022-12-08 10:27:16 -0300

    Add JWT authentication

--- src/main/java/com/jojo/financialcontrol/FinancialControlApplication.java
+++ src/main/java/com/jojo/financialcontrol/FinancialControlApplication.java
@@ -1,9 +1,12 @@
 package com.jojo.financialcontrol;
 
+import com.jojo.financialcontrol.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class FinancialControlApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jojo/financialcontrol/config/DataBaseConfig.java
+++ src/main/java/com/jojo/financialcontrol/config/DataBaseConfig.java
@@ -3,30 +3,13 @@ package com.jojo.financialcontrol.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.TransactionManager;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.jojo.financialcontrol.repository")
 public class DataBaseConfig {
-
-//    @Bean
-//    public EntityManagerFactory entityManagerFactory() {
-//
-//        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-//        factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-//        factory.setPackagesToScan("com.jojo.financialcontrol.entity");
-//        factory.setDataSource(dataSource());
-//        factory.afterPropertiesSet();
-//
-//        return factory.getObject();
-//    }
 
     @Bean
     public DataSource dataSource() {
@@ -40,14 +23,5 @@ public class DataBaseConfig {
         return dataSource;
 
     }
-
-//    @Bean
-//    public TransactionManager transactionManager() {
-//
-//        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
-//        transactionManager.setDataSource(dataSource());
-//
-//        return transactionManager;
-//    }
 
 }
