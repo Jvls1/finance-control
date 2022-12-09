aff176e7dd8a64f3208cc033cec1bd7eab0ce547
joao <joaovlopes000@gmail.com>
2022-12-09 13:52:55 -0300

    UUID generator add on the base entity

--- src/main/java/com/jojo/financialcontrol/entity/generic/BaseEntity.java
+++ src/main/java/com/jojo/financialcontrol/entity/generic/BaseEntity.java
@@ -1,7 +1,9 @@
 package com.jojo.financialcontrol.entity.generic;
 
+import com.jojo.financialcontrol.utils.UuidGenerator;
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
@@ -11,7 +13,8 @@ import java.util.UUID;
 public abstract class BaseEntity implements Serializable {
 
     @Id
-    @GeneratedValue
+    @GenericGenerator(name="uuid_generator", strategy = UuidGenerator.STRATEGY_PATH)
+    @GeneratedValue(generator = "uuid_generator")
     private UUID id;
 
 }
