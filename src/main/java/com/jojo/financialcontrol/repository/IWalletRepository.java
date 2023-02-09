787de68f336949b830571dbef159c6793235e762
joao <joaovlopes000@gmail.com>
2023-02-09 16:00:08 -0300

    fixed import

--- src/main/java/com/jojo/financialcontrol/repository/IWalletRepository.java
+++ src/main/java/com/jojo/financialcontrol/repository/IWalletRepository.java
@@ -1,6 +1,5 @@
 package com.jojo.financialcontrol.repository;
 
-import com.jojo.financialcontrol.entity.User;
 import com.jojo.financialcontrol.entity.Wallet;
 import com.jojo.financialcontrol.repository.generic.IGenericRepository;
 import org.springframework.stereotype.Repository;
