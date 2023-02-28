d70cd4794b51ac8f41f9a269eed95603840b5827
João Vitor <joaovlopes000@gmail.com>
2023-02-28 19:15:13 -0300

    AuthenticationProvider

---
+++ src/main/java/com/jojo/financialcontrol/config/FinanceControlAuthenticationProvider.java
@@ -0,0 +1,38 @@
+package com.jojo.financialcontrol.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class FinanceControlAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        // TODO: implement
+        return null;
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities() {
+        // TODO: implement
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
