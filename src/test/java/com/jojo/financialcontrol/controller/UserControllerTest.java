22588badf54c3cb09654b67e3ccb98255f1a4820
João Vitor <joaovlopes000@gmail.com>
2023-03-01 21:44:10 -0300

    default config unit test

--- src/test/java/com/jojo/financialcontrol/controller/UserControllerTest.java
+++ src/test/java/com/jojo/financialcontrol/controller/UserControllerTest.java
@@ -1,7 +1,17 @@
 package com.jojo.financialcontrol.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
-public class UserControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserControllerTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    int randomServerPort;
+
 }
