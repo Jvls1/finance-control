d68a9d8372780d2d6f693c155455057745d743a6
João Vitor <joaovlopes000@gmail.com>
2023-03-02 21:49:19 -0300

    add missing imports

--- src/test/java/com/jojo/financialcontrol/controller/UserControllerTest.java
+++ src/test/java/com/jojo/financialcontrol/controller/UserControllerTest.java
@@ -1,8 +1,10 @@
 package com.jojo.financialcontrol.controller;
 
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
