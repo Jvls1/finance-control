891fdd6d8f16329b8994bf508104084e3d00e16c
João Vitor <joaovlopes000@gmail.com>
2023-03-15 21:59:19 -0300

    - add findAll test wallet

--- src/test/java/com/jojo/financialcontrol/controller/ExpenseControllerTest.java
+++ src/test/java/com/jojo/financialcontrol/controller/ExpenseControllerTest.java
@@ -1,12 +1,37 @@
 package com.jojo.financialcontrol.controller;
 
+import com.jojo.financialcontrol.repository.IUserRepository;
+import com.jojo.financialcontrol.utils.Routes;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 public class ExpenseControllerTest extends BaseTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    int randomServerPort;
+
+    @Autowired
+    private IUserRepository iUserRepository;
 
     @Test
     void findAll() {
+        final String requestUrl = "http://localhost:" + randomServerPort + Routes.WALLET;
+
+        HttpEntity<Object> request = new HttpEntity<>(getJwtAuthenticationHeader());
+
+        ResponseEntity<String> result = restTemplate.exchange(requestUrl, HttpMethod.GET, request, String.class);
+        Assertions.assertEquals(201, result.getStatusCode().value());
     }
 
     @Test
