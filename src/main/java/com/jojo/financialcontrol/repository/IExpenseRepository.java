d425ab34c47d0a9a4e92a059a2a95a67b8b40cb7
João Vitor <joaovlopes000@gmail.com>
2023-03-24 19:52:23 -0300

    - new query impl

--- src/main/java/com/jojo/financialcontrol/repository/IExpenseRepository.java
+++ src/main/java/com/jojo/financialcontrol/repository/IExpenseRepository.java
@@ -1,9 +1,17 @@
 package com.jojo.financialcontrol.repository;
 
 import com.jojo.financialcontrol.model.Expense;
+import com.jojo.financialcontrol.model.Wallet;
 import com.jojo.financialcontrol.repository.generic.IGenericRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Month;
+import java.time.Year;
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 public interface IExpenseRepository extends IGenericRepository<Expense> {
+
+    List<Expense> findExpensesByWalletId(UUID idWallet);
 }
