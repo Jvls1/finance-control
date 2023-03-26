14b386dd75bc99de359b45a6af599eaefe0c2c2e
João Vitor <joaovlopes000@gmail.com>
2023-03-26 20:30:34 -0300

    - find all wallets by page

--- src/main/java/com/jojo/financialcontrol/controller/WalletController.java
+++ src/main/java/com/jojo/financialcontrol/controller/WalletController.java
@@ -7,6 +7,7 @@ import com.jojo.financialcontrol.model.to.WalletCreationTO;
 import com.jojo.financialcontrol.service.WalletServiceImpl;
 import com.jojo.financialcontrol.constants.Routes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,18 +23,18 @@ public class WalletController {
     private final WalletServiceImpl walletService;
 
     @GetMapping()
-    public ResponseEntity<Object> getWalletAll() {
-        Optional<Wallet> wallet = walletService.findById(idWallet);
-        if (wallet.isEmpty()) {
-            return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+    public ResponseEntity<Page<Wallet>> getWalletAll(@RequestParam Integer page, @RequestParam Integer row) throws InfoNotFoundException {
+        Page<Wallet> walletPage = walletService.findAll(page, row);
+        if (walletPage.isEmpty()) {
+            throw new InfoNotFoundException("Wallet not found");
         }
-        return new ResponseEntity<>(wallet.get(), HttpStatus.OK);
+        return new ResponseEntity<>(walletPage, HttpStatus.OK);
 
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getWalletById(@PathVariable("id") UUID idWallet) {
-        Optional<Wallet> wallet = walletService.findAll(idWallet);
+        Optional<Wallet> wallet = walletService.findById(idWallet);
         if (wallet.isEmpty()) {
             return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
         }
